c5bde58e36f930b3de450b49bc2819e56472ae40
atmshiblesadik <shiblesadik@outlook.com>
2019-12-09 19:42:09 +0600

    Solve Problem A from 2008 Round Q

--- 2008/Round Q/A.cpp
+++ 2008/Round Q/A.cpp
@@ -1,25 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 int test = 0, tc = 0;
-int search_engine, quries, answer;
-char search_engine_name[101][101];
-char query_search_engine[101][101];
+int search_engine, quries, answer, total = 0, upper = 0;
+int search_engine_counter[101];
+string engine_name;
+map<string, int> engine_id;
 int main(void) {
-    scanf("%d", &test);
+    cin >> test;
     while (test--) {
-        scanf("%d ", &serach_engine);
+        cin >> search_engine;
+        engine_id.clear();
+        getline(cin, engine_name);
         for (int i = 0; i < search_engine; ++i) {
-            gets(search_engine_name[i]);
+            getline(cin, engine_name);
+            engine_id[engine_name] = i;
         }
-        scanf("%d ", &quries);
-        for (int i = 0; i < quries; ++i) {
-            gets(query_search_engine[i]);
-        }
+        cin >> quries;
+        getline(cin, engine_name);
+        memset(search_engine_counter, 0, sizeof(search_engine_counter));
         answer = 0;
-
+        for (int i = 0; i < quries; ++i) {
+            getline(cin, engine_name);
+            search_engine_counter[engine_id[engine_name]] = 1;
+            total = 0;
+            for (int j = 0; j < search_engine; ++j) {
+                if (search_engine_counter[j]) ++total;
+            }
+            if (total == search_engine) {
+                ++answer;
+                memset(search_engine_counter, 0, sizeof(search_engine_counter));
+            }
+            search_engine_counter[engine_id[engine_name]] = 1;
+        }
         printf("Case #%d: %d\n", ++tc, answer);
     }
-
     return 0;
 }
-
